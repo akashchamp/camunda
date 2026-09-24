56896118b0d22a17f09a9e123fb828070c459964
Jonathan Ballet <jonathan.ballet@camunda.com>
2026-09-24 17:33:56 +0200

    fix: add cleanup step back
    Will be removed once our own resources are completely gone from the keycloak-operator namespace

--- .github/scripts/clean-up-load-test-namespaces.sh
+++ .github/scripts/clean-up-load-test-namespaces.sh
@@ -41,6 +41,11 @@ do
       echo "[DRY-RUN] Would delete namespace: $ns (deadline: $deadlineDate)"
     else
       kubectl delete namespace "$ns" --wait=false
+      # Also delete the Keycloak instance and its Secrets in the keycloak-operator
+      # namespace. This could be removed once Keycloak instances can be part of
+      # the same namespace as the load test itself.
+      # TODO: remove once no Keycloak deployment is left in the keycloak-operator namespace.
+      kubectl delete keycloak,secret --ignore-not-found --namespace keycloak-operator --selector "camunda.io/load-test-namespace=$ns"
     fi
     namespacesDeleted+=" * $ns (deadline: $deadlineDate)\n"
   else
